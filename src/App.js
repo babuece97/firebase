2b56a1c8c5cc19f300159e3990efb28d06a6cff8
babuece97 <babuece97@yahoo.com>
2021-03-15 00:08:38 +0100

    41-7

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import  firebase from "firebase/app";
@@ -6,21 +7,46 @@ import firebaseConfig from './FirebaseConfig';
 firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [user, setUser] = useState({
+    isSignedIn:false,
+    name:'',
+    email:'',
+    photo:'',
+  })
+
   const provider = new firebase.auth.GoogleAuthProvider();
   const handeleSignInn = ()=>{
     firebase.auth().signInWithPopup(provider)
   .then(result => {
     const {displayName, photoURL, email}= result.user;
+    const signedInUser = {
+      isSignedIn: true,
+      name: displayName,
+      email:email,
+      photo: photoURL
+    }
+    setUser(signedInUser);
     console.log(displayName, photoURL, email);
     // Google Auth is done
-
+  })
+  .catch(Errorr=> {
+    console.log(Errorr);
+    console.log(Errorr.messagee);
   })
 }
 
   return (
     <div className="App">
-      <button onClick={handeleSignInn}>signinn</button>
       <h1> FireBasee</h1>
+      <button onClick={handeleSignInn}>signinn</button>
+      {
+        user.isSignedIn && <div>
+        <p> Welcomee, {user.name}</p>
+        <p> Your Email: {user.email}</p>
+        <img src={user.photo} alt=""/> 
+        </div>
+      }
+      
     </div>
   );
 }
