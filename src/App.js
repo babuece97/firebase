42bbee2482e3d0c581bb97ac463a3f2fc0adc380
babuece97 <babuece97@yahoo.com>
2021-03-14 02:19:41 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import  firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './FirebaseConfig';
+firebase.initializeApp(firebaseConfig);
+
+function App() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handeleSignInn = ()=>{
+    firebase.auth().signInWithPopup(provider)
+  .then(result => {
+    const {displayName, photoURL, email}= result.user;
+    console.log(displayName, photoURL, email);
+
+  })
+}
+
+  return (
+    <div className="App">
+      <button onClick={handeleSignInn}>signinn</button>
+      <h1> FireBasee</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FirebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDN8mifjmvTQPFyPtY5FgY1LBuaPoY1y3g",
+    authDomain: "ema-jon-demo.firebaseapp.com",
+    projectId: "ema-jon-demo",
+    storageBucket: "ema-jon-demo.appspot.com",
+    messagingSenderId: "489049938930",
+    appId: "1:489049938930:web:9ebcc2b33f52539c14696b"
+  };
+  export default firebaseConfig;
