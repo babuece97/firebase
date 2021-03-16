afb90c4e2b37a02f24d51aec3a774627d6c704b2
babuece97 <babuece97@yahoo.com>
2021-03-16 20:10:22 +0100

    42-4

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
     name:'',
     email:'',
     photo:'',
+    password:''
   })
 
   const provider = new firebase.auth.GoogleAuthProvider();
@@ -55,18 +56,24 @@ const handleSignOut = () => {
 }
 
 const handleBlur = (e) => {
-console.log(e.target.name, e.target.value);
-if (e.target.name === 'Email'){
-  const isEmailValid = /\S+@\S+\.\S+/.test(e.target.value);
-  console.log(isEmailValid)
-
+  debugger;
+let isFormValid = true;   
+if (e.target.name === 'email'){
+   isFormValid = /\S+@\S+\.\S+/.test(e.target.value); 
 }
+
 if (e.target.name === 'password'){
   const isPasswordValid = e.target.value.length>6;
   const passwordHasNumber = /\d{1}/.test(e.target.value);
-  console.log(isPasswordValid && passwordHasNumber);
+  isFormValid = isPasswordValid && passwordHasNumber;
 
  } 
+ if(isFormValid){
+   const newUserInfo = {...user};
+   newUserInfo[e.target.name] = e.target.value;  
+   setUser(newUserInfo);
+
+ }
 }
 const handleSubmit = () => {
 
@@ -81,15 +88,23 @@ const handleSubmit = () => {
       }
       {
         user.isSignedIn && <div>
-        <p> Welcomee, {user.name}</p>
+        
         <p> Your Email: {user.email}</p>
+        <p> Passsword: {user.password}</p>
         <img src={user.photo} alt=""/> 
         </div>
       }
 
     <h1>Our Authentication System</h1>
+    <p>User Namee:{user.name}</p>
+    <p>Emaill:{user.email}</p>
+    <p>Passwprdd:{user.password}</p>
     <form onSubmit= {handleSubmit}> 
-    <input type="text" name="Email" onBlur={handleBlur} placeholder="Write your emailID" required/>
+    
+  
+    <input name="name" type="text" onBlur={handleBlur} placeholder="YOUR name"/> 
+    <br/>
+    <input type="text" name="email" onBlur={handleBlur} placeholder="Write your emailID" required/>
     <br/>
     <input type="password" name="password" onBlur = {handleBlur} placeholder="your pwd" required/>
     <br/>
